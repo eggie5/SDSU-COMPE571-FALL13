4ce2bfb195ce0f2aae96c3c912413c6c57f2e806
Alex Egg <eggie5@gmail.com>
2013-09-27 11:02:40 -0700

    simplified looping

--- data_generator/data_generator/main.c
+++ data_generator/data_generator/main.c
@@ -14,10 +14,12 @@
 #include <sys/types.h>
 
 #define BUFLEN 1514
-#define MAX_PACKET 11776 //bits
-#define OVERHEAD 20 //approx overhead for sendto return/usecond for this env. MAX is 40ish
+#define MAX_PACKET 11776 //bits -- 1.4KB
+#define OVERHEAD 35 //approx overhead for sendto return/usecond for this env. MAX is 40ish
 #define SRV_IP "127.0.0.1"
 #define PORT 32000
+#define MICRO_PER_SECOND	1000000
+#define MILLION 1000000
 //30 us is the min
 
 static int *glob_var;
@@ -26,19 +28,17 @@ int main(int argc, char** argv)
 {
     //he wants 350 max
     //200mbps seems to be my max on 1 process
-    int r=2000; //mbps form terminal arg
+    int r=201; //mbps form terminal arg
     int t=10; //seconds form terminal arg
     int packet_count=0;
     
-    glob_var = mmap(NULL, sizeof *glob_var, PROT_READ | PROT_WRITE,
-                    MAP_SHARED | MAP_ANON, -1, 0);
-    *glob_var = 0;
+    printf("r=%d t=%d\n", r,t);
     
     //socket stuff
     struct sockaddr_in si_other;
     int slen=sizeof(si_other);
     
-    int _socket, i;
+    int _socket;
 
     char buf[BUFLEN];
     
@@ -58,22 +58,26 @@ int main(int argc, char** argv)
     }
     
     
-    //APP logic
-    int r_bits = r * pow(10, 6);
+    //App. logic
+    int r_bits = r * MILLION;
     
-    int workers = ceil(r / (float)300);
+    int workers = ceil(r / (float)200);
     printf("launching %d worker(s)\n", workers);
 
     int num_of_packets = r_bits / MAX_PACKET ;
     num_of_packets = num_of_packets/workers; //packets/worker
     
-    double sleep_time = (1 / (double)num_of_packets) * pow(10,6); //sleep in us
+    double sleep_time = (1 / (double)num_of_packets) * MILLION; //sleep in us
     sleep_time= sleep_time - OVERHEAD ;
     
     if(sleep_time <1)
     {
         printf("neg sleep time: %lf, exiting", sleep_time);
         return 0;
+    }
+    if(sleep_time<30)
+    {
+        printf("WARNING: sleeep time is < 30us -- this percission is not reliable\n");
     }
     
     printf("%d / %d = %d, sleep=%lf\n", r_bits, MAX_PACKET, num_of_packets, sleep_time);
@@ -82,29 +86,37 @@ int main(int argc, char** argv)
     {
         if (fork()==0)
         {
-            printf("i am child, pid=%d\n", getpid());
+            printf("child started: pid=%d\n", getpid());
+            struct timeval start_time;
+            struct timeval stop_time;
+            float drift;
+            gettimeofday( &start_time, NULL );
             
             //do work
-            for(i=0; i<t; i++)
+            for(;;)
             {
-                for(int i=0; i< num_of_packets; i++)
-                {
-                    if (sendto(_socket, buf, BUFLEN, 0, (struct sockaddr *)&si_other, slen)==-1)
-                        printf("Error: sendto()\n");
+                if (sendto(_socket, buf, BUFLEN, 0, (struct sockaddr *)&si_other, slen)==-1)
+                    printf("Error: sendto()\n");
                     
-                    packet_count++;
-                    *glob_var=*glob_var+1;
-                    //printf("sleeping...\n");
-                    usleep(sleep_time);
-                }
+                packet_count++;
+                usleep(sleep_time);
+                
+                gettimeofday( &stop_time, NULL );
+                
+                drift = (float)(stop_time.tv_sec  - start_time.tv_sec);
+                drift += (stop_time.tv_usec - start_time.tv_usec)/(float)MICRO_PER_SECOND;
+                
+                if(drift>=(double) t)
+                    break;
+                
             }
             
-            printf("child %d done - %d packets sent\n", getpid(), packet_count);
+            printf("child closed: %d - %d packets sent\n", getpid(), packet_count);
             return 0;
         }
         else
         {
-            printf("i am parent, pid=%d\n", getpid());
+            //printf("i am parent, pid=%d\n", getpid());
         }
     }
     
@@ -112,7 +124,6 @@ int main(int argc, char** argv)
     close(_socket);
     wait(NULL);
     
-    printf("\npacket_count=%d\n", *glob_var);
     printf("exiting main\n");
     return 0;
 
