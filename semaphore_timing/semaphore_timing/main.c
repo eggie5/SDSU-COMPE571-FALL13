cb7bbfd9d1b2e656683e0ef107e48af1e9fdc6ba
Alex Egg <eggie5@gmail.com>
2013-10-07 12:20:49 -0700

    added semaphore project

---
+++ semaphore_timing/semaphore_timing/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  semaphore_timing
+//
+//  Created by Alex Egg on 10/7/13.
+//  Copyright (c) 2013 Alex Egg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
